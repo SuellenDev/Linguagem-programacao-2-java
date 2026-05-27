9589b5bf570befeab2847e75ada433b887958a75
Suellen Araújo <contatosusuaver@gmail.com>
2026-05-26 23:31:17 -0300

    List of Exercises 1

---
+++ lista-exercicios1.java
@@ -0,0 +1,46 @@
+/*
+1) O que é um paradigma de programação?
+R: É um conjunto de regras que dita como você deve pensar, organizar e executar instruções dp programa.
+
+2) Cite os principais paradigmas da programação.
+R: Os principais paradigmas da programação são: Orientação a Objetos, Funcional, Imperativo e Declarativo.
+Imperativo: "Como" deve ser feito.
+Declarativo: "O que" deve ser feito.
+Orientação a Objetos: Organiza o programa em objetos que possuem dados e ações.
+Funcional: Organiza o programa em funções puras, evitando alterar estados externos.
+
+3) O que é programação orientada a objetos? E quais suas vantagens?
+R: POO, é um paradigma que organiza o desenvolvimento de softwares em torno de dados ou objetos. As vantagens tá na organização novamente e manutenção do sistema.
+
+4) O que é uma abstração?
+R: É o ato de esconder detalhes e mostrar apenas o que é necessário para o usuário.
+
+5) O que é uma classe?
+R: É como se fosse um molde para criar objetos.
+
+Exemplos)
+Carro:
+caracteristica: Cor, Marca, Ano, Potencia.
+comportamento: Acelar, Frear, Girar, Virar.
+
+Celular:
+caracteristica: Marca, Modelo, Memoria, Processador.
+comportamento: Ligar, Desligar, Enviar Mensagem, Receber Chamada.
+
+Cão:
+caracteristica: Raca, Nome, Idade, Peso.
+comportamento: Latir, Comer, Dormir.
+
+Professor:
+caracteristica: Nome, Idade, Sexo, Disciplina.
+comportamento: Ensinar, Avaliar, Atender.
+
+Televisão:
+caracteristica: Marca, Modelo, Tamanho, Resolucao.
+comportamento: Ligar, Desligar, Mudar Canal, Ajustar Volume.
+
+Disquete:
+caracteristica: Marca, Modelo, Tamanho, Peso.
+comportamento: Abrir, Fechar, Escrever, Ler.
+
+*/
