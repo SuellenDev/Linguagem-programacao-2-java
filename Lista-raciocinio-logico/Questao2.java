e3fae6a12dc36bd2130d7555abb41f56a6ba9c8f
Suellen Araújo <contatosusuaver@gmail.com>
2026-05-27 22:57:35 -0300

    Add gasoline cost calculator program
    The program calculates the amount of gasoline needed and its cost based on user input for distance and gasoline price.

---
+++ Lista-raciocinio-logico/Questao2.java
@@ -0,0 +1,24 @@
+//Construa um algoritmo que calcule e informe a quantidade de gasolina que será preciso colocar no carro e quanto irá custar para o seu dono ir até a sua fazenda. O usuário deverá informar a distância que será percorrida e o preço do litro da gasolina. Para o calculo, sabe-se que o carro faz em média 12 Km/litro.
+import java.util.Scanner;
+
+public class Main {
+public static void main(String [] args){
+  
+  Scanner input = new Scanner (System.in);
+  double lGasolinaNecessaria;
+  double distanciaPercorrida;
+  double precoLitroG;
+  double valorFinal;
+
+  System.out.println("Qual a distância que você vai percorrer?");
+  distanciaPercorrida = input.nextDouble();
+  System.out.println("Quanto é o valor do litro de gasolina?");
+  precoLitroG = input.nextDouble();
+  lGasolinaNecessaria = distanciaPercorrida / 12;
+  valorFinal = lGasolinaNecessaria * precoLitroG;
+
+  System.out.printf("A quantidade de gasolina necessária é: %.2f\n", lGasolinaNecessaria);
+  System.out.printf("Vai custar : R$%.2f\n", valorFinal);
+  
+}
+}
