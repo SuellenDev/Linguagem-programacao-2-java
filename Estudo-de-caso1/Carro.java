cc7d2983fc4ba790425ee512faf0aca3a35cc5ca
Suellen Araújo <contatosusuaver@gmail.com>
2026-05-28 17:10:07 -0300

    Create Carro class with user input functionality
    Implement initial structure for Carro class with user input.

---
+++ Estudo-de-caso1/Carro.java
@@ -0,0 +1,19 @@
+//Faça um algoritmo utilizando a representação de fluxograma que solicite ao usuário dois valores numéricos. Em seguida o algoritmo deverá efetuar a soma dos dois valores e caso o resultado seja maior ou igual a 10, deve-se ser somado a 5 caso contrário o valor do resultado deverá ser subtraído de 7. Após a obtenção do resultado apresente essa informação para o usuário. import java.util.Scanner;
+import java.util.Scanner;
+public class Carro {
+public static void main(String [] args){
+  
+  Scanner input = new Scanner (System.in);
+  String modelo;
+  double velocidade;
+  double aceleracao;
+  int marcha;
+
+  public Carro 
+
+  
+
+
+  input.close();
+}
+}
