6f2e3b20fdd34541939a2ea4f8a6254be38797b0
Suellen Araújo <contatosusuaver@gmail.com>
2026-05-27 22:10:58 -0300

    Fix variable naming and improve salary calculation

--- Lista-raciocinio-logico/Questao1.java
+++ Lista-raciocinio-logico/Questao1.java
@@ -1,12 +1,18 @@
 //Contruir um algoritmo que solicite ao usuário a quantidade de horas trabalhadas no mês por um operário. Em seguida, calcular e informar o seu salãrio sabendo que ele ganha R$20,00 por hora.
 import java.util.Scanner;
+
 public class Main {
 public static void main(String [] args){
+  
   Scanner input = new Scanner (System.in);
+  double horasTb;
+  double finalSalario;
+  
   System.out.println("Informe a quantidade de horas trabalhadas no mês: ");
-  double horastb = input.nextDouble();
-  double finalSalario = 20 * horastb;
+  horasTb = input.nextDouble();
+  finalSalario = 20 * horasTb;
   System.out.println("O salário no mês foi: R$ " + finalSalario);
   input.close();
 }
 }
+
