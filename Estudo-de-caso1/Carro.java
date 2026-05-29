9cc57ce5f1951c1b73192a09cd9cea52a80c1d93
Suellen Araújo <contatosusuaver@gmail.com>
2026-05-29 00:50:13 -0300

    Refactor Carro class and clean up code
    Refactor Carro class constructor and remove comments.

--- Estudo-de-caso1/Carro.java
+++ Estudo-de-caso1/Carro.java
@@ -1,4 +1,3 @@
-//Faça um algoritmo utilizando a representação de fluxograma que solicite ao usuário dois valores numéricos. Em seguida o algoritmo deverá efetuar a soma dos dois valores e caso o resultado seja maior ou igual a 10, deve-se ser somado a 5 caso contrário o valor do resultado deverá ser subtraído de 7. Após a obtenção do resultado apresente essa informação para o usuário. import java.util.Scanner;
 import java.util.Scanner;
 public class Carro {
 public static void main(String [] args){
@@ -9,11 +8,12 @@ public static void main(String [] args){
   double aceleracao;
   int marcha;
 
-  public Carro 
+  public Carro {
 
-  
+    
+  }
 
-
+  não estou conseguindo raciocinar de tanto sono, inferno de materia
   input.close();
 }
 }
