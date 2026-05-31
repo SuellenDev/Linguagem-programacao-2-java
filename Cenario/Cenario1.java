3a6c82265c0eb95654c2592216e284ed0a118d1c
Suellen Araújo <contatosusuaver@gmail.com>
2026-05-30 23:37:39 -0300

    Add initial structure for Cenario1 class

---
+++ Cenario/Cenario1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class void Main{
+  public static void main(String[] args){
+    String nome;
+    private float preco;
+    private int quantidadeEstoque;
+    static int contador;
+
+    public Produto(String nomeProduto, float precoProduto, int quantidadeEstoqueProduto, int contadorProduto){
+      this.nome = nomeProduto;
+      this.preco = precoProduto;
+      this.quantidadeEstoque = quantidadeEstoqueProduto;
+      this.contador = contadorProduto;
+      
+    }
+
+    public Pr                                   
+  }
+}
