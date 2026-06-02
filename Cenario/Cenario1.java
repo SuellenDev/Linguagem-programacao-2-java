52fdafa8d0082fafd3b066631d133fc241f87b72
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-01 23:35:28 -0300

    Change variable access modifiers and add price check
    Refactor variables to private access and add validation for price.

--- Cenario/Cenario1.java
+++ Cenario/Cenario1.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 
 public class void Main{
   public static void main(String[] args){
-    String nome;
+    private String nome;
     private float preco;
     private int quantidadeEstoque;
-    static int contador;
+    private static int contador;
 
     public Produto(String nomeProduto, float precoProduto, int quantidadeEstoqueProduto, int contadorProduto){
       this.nome = nomeProduto;
       this.preco = precoProduto;
       this.quantidadeEstoque = quantidadeEstoqueProduto;
       this.contador = contadorProduto;
+
+      if (preco < 0){
+        system.out.println("");
+      }
       
     }
 
