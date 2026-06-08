80068dc9348fee8eda6ae341fb57c3d3ef0de893
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-07 23:39:59 -0300

    Refactor Calculo class and add main method
    Refactor Calculo class and implement main method to calculate salary.

--- listaAtividade/calculo.java
+++ listaAtividade/calculo.java
@@ -1,9 +1,20 @@
-import java.util.Scanner
+public class Main {
 
-public class Calculo{
-  private totalSalario;
-  private double salario;
-  private double horasTrabalhadas;
+  public static void main(String[] args) {
+      Calculo calculo = new Calculo();
+      calculo.calculoSalario(58, 20);
+      
+      System.out.println("O salário final é: R$ " + calculo.getTotalSalario());
+    }
+}
+class Calculo {
+    private double totalSalario;
 
-  
+    public void calculoSalario(double salario, int horasTrabalhadas) {
+        totalSalario = salario * horasTrabalhadas;
+      
+    }
+    public double getTotalSalario() {
+        return totalSalario;
+    }
 }
