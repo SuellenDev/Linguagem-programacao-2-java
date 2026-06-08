a9d0e11f0988e39f485ba6ee0d5f6bc1b84d7f91
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-08 01:13:41 -0300

    Add Funcionario class with basic attributes

---
+++ listaAtividade/funcionario.java
@@ -0,0 +1,11 @@
+class Funcionario{
+  private String nome;
+  private int CPF;
+  private int RG;
+  private int matricula;
+  private double salario;
+  private int geraMatricula = 0;
+
+  public Funcionario(String nome, double salario, double horasTrabalhadas)
+  
+}
