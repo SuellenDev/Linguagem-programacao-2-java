c8a9752ac769e8f5d843d4250ac88b6a3dbdccd0
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-09 00:28:43 -0300

    Revise activity 1 with composition and aggregation details
    Updated activity analysis with classifications and justifications.

---
+++ Lista de atividades II-0206/atividade1.java
@@ -0,0 +1,20 @@
+/*Atividade 1 – Análise Conceitual: Composição ou
+Agregação
+Classifique os relacionamentos abaixo como Composição ou Agregação e
+justifique:
+a) Universidade e Departamentos
+b) Computador e Processador
+c) Professor e Projetor
+d) Carro e Motor
+e) Cliente e Endereços
+
+A) Em dúvida
+B) Composição, pois todo computador deve haver o processador para funcionar.
+C) Agregação, pois o projetor é independente, nem todo professor utiliza projetor.
+D) Composição, na construção mecânica de um carro deve conter o motor.
+E) Que tipo de endereço? Endereço de rede?
+
+
+
+
+*/
