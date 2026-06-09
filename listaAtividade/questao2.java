7ea7de518d0347d3c170af0331e19071c8dbc938
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-09 08:32:37 -0300

    Document usage of Funcionario class in questao2.java
    Add comments explaining the functionality of the 'Funcionario' class and its usage.

---
+++ listaAtividade/questao2.java
@@ -0,0 +1,48 @@
+/*
+Funcionário, com base nela responda as perguntas abaixo.
+1 public class Programa {
+23
+public static void main(String[] args)
+4 {
+5 Funcionario f1 = new Funcionario("Zé",25,44);
+67
+f1.numRG ="12345678";
+8 f1.cpf ="123456789101";
+9 f1.salario = 380;
+10 f1.matricula = 123;
+11 f1.calculaSalario();
+12 f1.imprimeDados();
+13
+14 Funcionario f2 = new Funcionario("Maria",25,40);
+15
+16 f2.numRG ="12345678";
+17 f2.cpf ="123456789101";
+18 f2.calculaSalario();
+19 f2.imprimeDados();
+20 }
+21 }
+a) O que acontece quando a ação da linha 5 é executada?
+b) Porque construção das linhas 7 e 8 é permitida?
+c) Nas linhas 9 e 10, acessamos os atributos salário e matricula da funcionário. Essa
+construção implementada nas linhas 9 e 10 é permitida pelo objeto funcionário?
+Justifique a sua resposta.
+
+A) É criado um novo funcionario com o nome zé do construtor da classe funcionario e mais dois parametros que acredito quer idade
+B) Se o numRG e cpf foram declarados como publico podem ser acessados e modificados diretamente por qualquer classe.
+C) Se encaixa na mesma situação da questão A.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
