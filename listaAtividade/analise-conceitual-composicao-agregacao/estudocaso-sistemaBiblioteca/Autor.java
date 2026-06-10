eb0b91bdfa7ee38d059a9753ca6f686c8b139982
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-09 23:48:34 -0300

    Add Autor class with attributes and constructor

---
+++ listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Autor.java
@@ -0,0 +1,12 @@
+public class Autor{
+  public string nome;
+  public string dataNascimento;
+  public string biografia;
+
+  public Autor(String nome, String dataNascimento, String biografia){
+    this.nome = nome;
+    this.dataNascimento = dataNascimento;
+    this.biografia = biografia
+    
+  }
+}
