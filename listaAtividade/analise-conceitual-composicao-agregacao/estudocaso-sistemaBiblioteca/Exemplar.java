eb082f469b4080a9d1d5e773d82ccd66cd74c6a8
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-10 00:13:38 -0300

    Add Exemplar class for library system

---
+++ listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Exemplar.java
@@ -0,0 +1,13 @@
+package sistemaBiblioteca;
+
+public class Exemplar{
+  public String categoria;
+  public String genero;
+  public String cdd
+
+  public Exemplar(String categoria, String genero, String cdd){
+    this.categoria = categoria;
+    this.genero = genero;
+    this.cdd = cdd;
+  }
+}
