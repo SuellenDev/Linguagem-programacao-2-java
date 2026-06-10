3a1c19a90313c5c7b8f8f51ba0ee066fe0938521
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-10 00:25:57 -0300

    Add Funcionario class with attributes and constructor

---
+++ listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Funcionario.java
@@ -0,0 +1,13 @@
+peckage sistemaBiblioteca;
+
+public class Funcionario{
+  public String nome;
+  public String cpf;
+  public String dataNascimento;
+
+  public Funcionario(String nome, String cpf, String dataNascimento){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.dataNascimento = dataNascimento;
+  }
+}
