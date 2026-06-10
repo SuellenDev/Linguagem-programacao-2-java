b73e1a4fea70ef12c86f2ae369bd8619ae8bdf6e
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-09 23:23:40 -0300

    Add analysis of Composition and Aggregation relationships
    This file contains an analysis of relationships between entities, classifying them as Composition or Aggregation with justifications.

---
+++ listaAtividade/analise-conceitual-composicao-agregacao/atividade-1.java
@@ -0,0 +1,23 @@
+/*Atividade 1 – Análise Conceitual: Composição ou
+Agregação
+Classifique os relacionamentos abaixo como Composição ou Agregação e
+justifique:
+a) Universidade e Departamentos
+b) Computador e Processador
+c) Professor e Projetor
+d) Carro e Motor
+e) Cliente e Endereços
+
+A) Composição, porém em dúvida.
+B) Composição, pois todo computador deve haver o processador para funcionar.
+C) Agregação, pois o projetor é independente, nem todo professor utiliza projetor.
+D) Composição, na construção mecânica de um carro deve conter o motor.
+E) Agregação, pois um cliente pode ter vários ou nenhum endereço.
+
+Explique o que acontece com os objetos componentes quando o objeto
+principal é destruído.
+
+Resposta: Ele não faz sentido sozinho, então é destruído automaticamente quando o principal também é.
+
+
+*/
