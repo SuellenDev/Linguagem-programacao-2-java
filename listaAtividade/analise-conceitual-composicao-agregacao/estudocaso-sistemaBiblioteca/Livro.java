aa7b05123547d91b880a5b38504a58102c3b3640
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-10 00:02:05 -0300

    Add Livro class for library system

---
+++ listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Livro.java
@@ -0,0 +1,18 @@
+peckage sistemaBiblioteca;
+
+public class Livro{
+  public String titulo;
+  public Autor autor;
+  public String dataPublicao;
+  public String descricao;
+  public String codigo;
+
+  public Livro(String titulo, Autor autor, String dataPublicacao, String descricao, String codigo){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.dataPublicacao = dataPublicacao;
+    this.descricao = descricao;
+    this.codigo = codigo;
+  }
+  
+}
