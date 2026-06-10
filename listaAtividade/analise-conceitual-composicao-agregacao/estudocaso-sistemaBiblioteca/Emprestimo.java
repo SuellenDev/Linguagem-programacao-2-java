7ab503381d63e33f00b66a50f89fdeaa2f7b1b98
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-10 01:13:15 -0300

    Add Emprestimo class for book loan management

---
+++ listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Emprestimo.java
@@ -0,0 +1,16 @@
+package sistemaBiblioteca;
+
+public class Emprestimo{
+  public Livro livro;
+  public String dataEmprestimo;
+  public String codigoEmprestimo;
+
+    public Emprestimo(Livro livro, String dataEmprestimo, String codigoEmprestimo){
+    this.statusLivro = statusLivro;
+    this.dataEmprestimo = dataEmprestimo;
+    this.codigoEmprestimo = codigoEmprestimo;
+    this.livro = livro;
+    this.livro.status = "Livre"
+}
+  
+}
