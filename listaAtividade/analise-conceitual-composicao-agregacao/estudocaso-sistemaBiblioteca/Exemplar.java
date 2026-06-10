ce9e03d8a0fe4b4ddb65d28d0729727281b2e09f
Suellen Araújo <contatosusuaver@gmail.com>
2026-06-10 16:10:12 -0300

    Fix missing semicolon in Exemplar class

--- listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Exemplar.java
+++ listaAtividade/analise-conceitual-composicao-agregacao/estudocaso-sistemaBiblioteca/Exemplar.java
@@ -3,7 +3,7 @@ package sistemaBiblioteca;
 public class Exemplar{
   public String categoria;
   public String genero;
-  public String cdd
+public String cdd;
 
   public Exemplar(String categoria, String genero, String cdd){
     this.categoria = categoria;
