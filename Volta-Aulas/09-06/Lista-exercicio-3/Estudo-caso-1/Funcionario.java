97a3a6acd8c593a72dc4b6faac404b5454a3aabe
Suellen Araújo <contatosusuaver@gmail.com>
2026-07-22 16:18:52 -0300

    Refactor Funcionario class and its subclasses

--- Volta-Aulas/09-06/Lista-exercicio-3/Estudo-caso-1/Funcionario.java
+++ Volta-Aulas/09-06/Lista-exercicio-3/Estudo-caso-1/Funcionario.java
@@ -1,23 +1,38 @@
-public class Funcionario {
-  private int nome;
-  private int CPF;
+public abstract class Funcionario {
+  private String nome;
+  private String cpf;
 
-  public Funcionario(int nome, int CPF){
+  public Funcionario(String nome, String cpf){
     this.nome = nome;
-    this.CPF = CPF;
+    this.cpf = cpf;
   }
+  public abstract double calcularPagamento();
+}
 
   public class FuncionarioHorista extends Funcionario{
     public int horasTrabalhadas;
     public int valorHora;
 
-    public FuncionarioHorista(int horasTrabalhadas, int valorHora){
+     public FuncionarioHorista(String nome, String cpf, int horasTrabalhadas, double valorHora) {
+      super(nome, cpf);
       this.horasTrabalhadas = horasTrabalhadas;
       this.valorHora = valorHora;
     }
-  }
-  
-  public void calcularPagamento(){
+      @Override
+    public double calcularPagamento() {
+        return horasTrabalhadas * valorHora;
+    }
+}
+
+  public class FuncionarioMensalista extends Funcionario{
+    private double salarioFixo;
     
-  }
+    public FuncionarioMensalista(String nome, String cpf, double salarioFixo) {
+        super(nome, cpf);
+      this.salarioFixo = salarioFixo;
+    }
+       @Override
+    public double calcularPagamento() {
+        return salarioFixo;
+    }
 }
